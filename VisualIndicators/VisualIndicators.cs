341ad1096a62ffa4ad0a0be058e6bef8b1392c3e
Jonathan Phelps <jphelps92@gmail.com>
2020-12-26 15:35:07 -0600

    Display chat message when shark or seagull attack

--- VisualIndicators/VisualIndicators.cs
+++ VisualIndicators/VisualIndicators.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
+using System;
 
 public class VisualIndicators : Mod
 {
+    AI_NetworkBehavior_Shark shark;
+    Seagull seagull;
+    DateTime? nextCheckTime;
+    readonly double timeout = 1000;
+
     public void Start()
     {
         Debug.Log("Mod VisualIndicators has been loaded!");
@@ -10,5 +16,43 @@ public class VisualIndicators : Mod
     public void OnModUnload()
     {
         Debug.Log("Mod VisualIndicators has been unloaded!");
+    }
+
+    public void Update()
+    {
+        // Don't try to do anything until we're in the game
+        if (Semih_Network.InMenuScene)
+        {
+            return;
+        }
+
+        if (nextCheckTime.HasValue && DateTime.Now < nextCheckTime)
+        {
+            return;
+        }
+
+        if (seagull == null)
+        {
+            seagull = FindObjectOfType<Seagull>();
+        }
+
+        // FindObjectOfType returns null during the loading screen so check for null
+        if (seagull != null && !seagull.IsKilled && seagull.currentState == SeagullState.Peck)
+        {
+            RAPI.BroadcastChatMessage("Seagull is eating plants!");
+        }
+
+        if (shark == null)
+        {
+            shark = FindObjectOfType<AI_NetworkBehavior_Shark>();
+        }
+
+        // FindObjectOfType returns null during the loading screen so check for null
+        if (shark != null && !shark.IsKilled && shark.stateMachineShark.biteRaftState.isBitingRaft == true)
+        {
+            RAPI.BroadcastChatMessage("Shark is attacking raft!");
+        }
+
+        nextCheckTime = DateTime.Now.AddMilliseconds(timeout);
     }
 }
