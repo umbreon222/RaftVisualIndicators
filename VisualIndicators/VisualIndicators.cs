08ec2650403ce11def0e751adf21426e95ac2862
Jonathan Phelps <jphelps92@gmail.com>
2020-12-26 14:31:04 -0600

    Add project files

---
+++ VisualIndicators/VisualIndicators.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class VisualIndicators : Mod
+{
+    public void Start()
+    {
+        Debug.Log("Mod VisualIndicators has been loaded!");
+    }
+
+    public void OnModUnload()
+    {
+        Debug.Log("Mod VisualIndicators has been unloaded!");
+    }
+}
