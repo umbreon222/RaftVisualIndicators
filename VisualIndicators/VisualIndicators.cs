806ca834b8d171caf27859759b57bd3aa9a71db2
Devin Wendt <devinwendt97@gmail.com>
2020-12-30 21:43:06 -0600

    Use a timer, simplify logic, add console switch

--- VisualIndicators/VisualIndicators.cs
+++ VisualIndicators/VisualIndicators.cs
@@ -1,58 +1,66 @@
-﻿using UnityEngine;
-using System;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Timers;
+using UnityEngine;
 
 public class VisualIndicators : Mod
 {
+    const double CHECK_INTERVAL = 1000;
+    public static VisualIndicators instance;
+    readonly Timer timer = new Timer(CHECK_INTERVAL);
+    bool modActive = true;
     AI_NetworkBehavior_Shark shark;
     Seagull seagull;
-    DateTime? nextCheckTime;
-    readonly double timeout = 1000;
 
     public void Start()
     {
         Debug.Log("Mod VisualIndicators has been loaded!");
+        instance = this;
+        timer.Elapsed += Timer_Elapsed;
     }
 
     public void OnModUnload()
     {
         Debug.Log("Mod VisualIndicators has been unloaded!");
+        timer.Stop();
     }
 
-    public void Update()
+    public override void WorldEvent_WorldLoaded()
     {
-        // Don't try to do anything until we're in the game
-        if (Semih_Network.InMenuScene)
+        Debug.Log("The world is loaded!");
+        // Find that pesky seagull
+        seagull = FindObjectOfType<Seagull>();
+        // Find that annoying shark
+        shark = FindObjectOfType<AI_NetworkBehavior_Shark>();
+        timer.Start();
+    }
+
+    private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        if (!modActive || seagull == null || shark == null)
         {
             return;
         }
-
-        if (nextCheckTime.HasValue && DateTime.Now < nextCheckTime)
-        {
-            return;
-        }
-
-        if (seagull == null)
-        {
-            seagull = FindObjectOfType<Seagull>();
-        }
-
-        // FindObjectOfType returns null during the loading screen so check for null
-        if (seagull != null && !seagull.IsKilled && seagull.currentState == SeagullState.Peck)
+        if (seagull.currentState == SeagullState.Peck)
         {
             RAPI.BroadcastChatMessage("Seagull is eating plants!");
         }
-
-        if (shark == null)
-        {
-            shark = FindObjectOfType<AI_NetworkBehavior_Shark>();
-        }
-
-        // FindObjectOfType returns null during the loading screen so check for null
-        if (shark != null && !shark.IsKilled && shark.stateMachineShark.biteRaftState.isBitingRaft == true)
+        if (shark.stateMachineShark.biteRaftState.isBitingRaft)
         {
             RAPI.BroadcastChatMessage("Shark is attacking raft!");
         }
-
-        nextCheckTime = DateTime.Now.AddMilliseconds(timeout);
     }
+
+    [ConsoleCommand(name: "visual_indicators", docs: "Turns visual indicators on or off.")]
+    [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Required by API")]
+    public static void ActivateVisualIndicators(string[] args)
+    {
+        if (instance != null)
+        {
+            // Flip the switch
+            instance.modActive = !instance.modActive;
+            Debug.Log(string.Format("Mod VisualIndicators is {0}!", instance.modActive ? "on" : "off"));
+        }
+    }
+
+    public void Update() {; }
 }
